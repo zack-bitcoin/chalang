ae24b25ac4100264aa6b17e4c8f60cb0551a69c6
zack <zack.bitcoin@gmail.com>
2016-12-12 15:04:25 +0100

    changed the test library so we can change the location of the test files

--- src/test_chalang.erl
+++ src/test_chalang.erl
@@ -1,23 +1,24 @@
 -module(test_chalang).
--export([test/0, run_script/2]).
+-export([test/0, test/1, run_script/3]).
 
 -define(loc, "examples/").
-run_script(X, Gas) ->
-    {ok, A} = file:read_file(?loc ++ X ++ ".fs"),
+run_script(X, Gas, Loc) ->
+    {ok, A} = file:read_file(Loc ++ X ++ ".fs"),
     io:fwrite("run script "),
     io:fwrite(A),
     io:fwrite("\n"),
     io:fwrite("\n"),
     B = compiler_chalang:doit(<<A/binary, <<"\n test \n">>/binary>>),
     {d,_,[<<1:32>>],_,_,_,_,_,_,_,_,_} = chalang:test(B, Gas, Gas, Gas, Gas).
-run_scripts([], _) -> ok;
-run_scripts([H|T], Gas) ->
-    {d, NewGas, [<<1:32>>],_,_,_,_,_,_,_,_,_} = run_script(H, Gas),
-    run_scripts(T, NewGas).
-test() ->
+run_scripts([], _, _) -> ok;
+run_scripts([H|T], Gas, Loc) ->
+    {d, NewGas, [<<1:32>>],_,_,_,_,_,_,_,_,_} = run_script(H, Gas, Loc),
+    run_scripts(T, NewGas, Loc).
+test() -> test(?loc).
+test(Loc) ->
     Scripts = ["hashlock", "function", "variable",
 	       "macro", "case", "recursion", "map"],
 	       %"merge"],
-    run_scripts(Scripts, 10000),
+    run_scripts(Scripts, 10000, Loc),
     success.
     
