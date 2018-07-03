8c01328aee89826ba2dfdd7ecaa40b8d567a0db7
zack <zack.bitcoin@gmail.com>
2018-07-03 11:30:54 -0400

    simplify map.scm

--- src/lisp2/map.scm
+++ src/lisp2/map.scm
@@ -13,21 +13,21 @@
 			 (recurse F (cdr X))))))))
 
 (=
-  (execute (map) ((double) (tree '(2 3 4 5))))
-  (tree '(4 6 8 10)))
+  (execute (map) ((double) (tree (2 3 4 5))))
+  (tree (4 6 8 10)))
 
 
 % this map is happening completely at compile-time
 (macro map2 (f l)
        (cond (((= () l) ())
 	      (true
-	       (cons (execute f (cons (car l) ()))
+	       (cons (execute f ((car l)))
 		     (map2 f (cdr l)))))))
 (macro fun (x) (* 2 x) )
 (macro test3 ()
        (=
-	(map2 'fun (cons 3 (cons 4 (cons 5 ()))))
-	(cons 6 (cons 8 (cons 10 ())))))
+	(map2 'fun (3 4 5))
+	(6 8 10)))
 (test3)
 
 and
