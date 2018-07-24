02fc4a8ceca7a8468e3169447056f8f8bf7b36ec
zack <zack.bitcoin@gmail.com>
2018-07-23 21:04:43 -0400

    started re-writing the market into lisp to make it easier to read, verify, and modify

---
+++ src/lisp/market.scm
@@ -0,0 +1,52 @@
+(import (tree_lib.scm cond_lib.scm oracle.scm))
+(macro mil () 1000000)
+
+;<<height:32, price:16, portionMatched:16, market_id:256, signature/binary>>
+(macro extract (X)
+;verify the signature.
+;check that the price declaration was made after the bet, or at the same height.       
+;return declaration_height, price and portion_matched
+       0)
+
+(macro contradictory_prices (SPD1 SPD2) ;return delay, nonce, amount
+	;extract both
+	;confirm that heights are within half a period of each other
+	;confirm that prices are unequal, or that the portion matched is unequal.
+	;(tree (0 (* 2 (mil)) 0))
+       0
+       )
+(macro no_publish ()
+       (tree ((@ Period)
+	      (/ height (* 2 (@ Period)))
+	      0)))
+(macro evidence (SPD) ;return delay, nonce, amount
+       ;extract it
+      ;require that SPD was made in the most recent period 
+       (tree ((- (@ Expires) height)
+	      (+ 1 (/ declaration_height (@ Period)))
+	      (- 10000 (@ MaxPrice)))))
+
+(macro match_order (SPD);return delay, nonce, amount
+;extract it
+       ;make sure it is better or equal to the agreed upon price.
+       ;run the oracle program.
+       ;if your bet was partly matched, then that needs to be handled differently.
+)
+(macro unmatched (OracleProof);return delay nonce amount
+       ;extract it
+       ;if the oracle is unresolved
+       (tree ((+ 2000 (+ (@ Expires) (@ Period)))
+	      0
+	      (- 10000 (@ MaxPrice))))
+;if the oracle is resolved
+       (tree ((@ Period)
+	      1
+	      (- 10000 MaxPrice))))
+
+(macro main (Type I1 I2)
+       '(cond (((= Type 0) (no_publish))
+	      ((= Type 1) (match_order I1))
+	      ((= Type 2) (contradictory_prices I1 I2))
+	      ((= Type 3) (evidence I1))
+	      ((= Type 4) (unmatched I1)))))
+       
---
+++ src/lisp/oracle.scm
@@ -0,0 +1,17 @@
+(import (eqs_lib.scm function_lib.scm cond_lib.scm tree_lib.scm))
+
+(macro oracle_result (Proof)
+  '(() 32 split drop 1 split swap drop --AAAA swap ++ )
+(macro oracle_result (ProofStructure); returns integer result of oracle.
+       '(oracle_result (cdr (car (car (cdr ProofStructure))))))
+(macro bet2 (R)
+       '(cond (((= R 1);oracle returns True
+	       ,(tree (0 3 (@ bet_amount ))))
+	      ((= R 2);oracle returns False
+	       ,(tree (0 3 (- 10000 (@ bet_amount)))))
+	      ((= R 3);oracle returns Bad Question
+	       ,(tree (0 3 (- 10000 (@ MaxPrice)))))
+	      ((= R 0);oracle is not yet closed
+	       ,(tree (1 1 (- 10000 (@ MaxPrice))))))))
+(macro bet (ProofStructure); returns delay, nonce, amount
+       (bet2 (oracle_result ProofStructure)))
