ad15226e6cdfcfb4df65e0b4d9b3733e706b280e
zack <zack.bitcoin@gmail.com>
2019-07-09 13:33:26 -0400

    added some comments to the example to explain how efficiently the different functions are getting compiled.

--- src/lisp/example.scm
+++ src/lisp/example.scm
@@ -18,18 +18,22 @@
 ;( A B -- Min )
 def 2dup < if drop else swap drop then end_fun
 forth_min ! ;defined a function to calculate the min of 2 integers.
+;8 opcodes
 
 ;( A B -- Max )
 def 2dup > if drop else swap drop then end_fun
 forth_max ! ;calculate the max of 2 integers
+;8 opcodes
 
 ;( A L -- Min )
 def nil === if drop drop else drop car@ tuck forth_min @ call swap recurse then end_fun
 smallest_in_list1 ! ;a function to calculate the minimum of a list of integers
+;15 opcodes
 
 ;( A L -- Max )
 def nil === if drop drop else drop car@ tuck forth_max @ call swap recurse then end_fun
 biggest_in_list1 ! ;a function to calculate the maximum of a list of integers
+;15 opcodes
 
 0 List @ dup tuck biggest_in_list1 @ call ;use the function to calculate the maximum of the list
 swap 0 swap
@@ -41,25 +45,31 @@ smallest_in_list1 @ call ;use the other function to calcualte the minimum of the
 ;next using lisp
 
 (define lispmax (a b);max of 2 integers
+  ;19 opcodes
   (cond (((> a b) a)
          (true b))))
 (define lispmin (a b);min of 2 integers
+  ;19 opcodes
   (cond (((< a b) a)
          (true b))))
 (define biggest_in_list2 (a L);max of a list
+  ;45 opcodes
   (cond (((= L nil) a)
          (true (recurse;you can use keyword `recurse` for recursion.
                 (lispmax a (car L))
                 (cdr L))))))
 (define smallest_in_list2 (a L);min of a list
+  ;45 opcodes
   (cond (((= L nil) a)
          (true (smallest_in_list2;you can use the name of the function for recursion.
                 (lispmin a (car L))
                 (cdr L))))))
 (define average (Q R)
+  ;3 opcodes
        (/ (+ Q R) 2))
 
 (define lisp_doit (L);putting it all together
+  ;23 opcodes
   ((average
    (biggest_in_list2 0 L)
    (smallest_in_list2 0 L))))
@@ -68,13 +78,17 @@ smallest_in_list1 @ call ;use the other function to calcualte the minimum of the
 
 
 ; now using lisp with generics
+; this version compiles to the shortest code out of the 4 examples we are looking at.
 
 (define biggest_in_list3 (L)
+  ;10 opcodes
   ((fold (@ forth_max) 0 L)))
    ;I used the function defined in forth syntax, to show how it is cross-compatible.
 (define smallest_in_list3 (L)
+  ;10 opcodes
   ((fold (@ lispmin) 0 L)));fold is a higher-order function that takes a pointer to another function as an input.
 (define generic_doit (L);putting it all together
+;19 opcodes
   ((average (biggest_in_list3 L)
            (smallest_in_list3 L))))
 
@@ -84,12 +98,16 @@ smallest_in_list1 @ call ;use the other function to calcualte the minimum of the
 ; now using something more similar to python syntax, where we allow for setting intermediate values Biggest and Smallest
 
 (deflet pythonic_doit (L)
+;41 opcodes
         ((Biggest (biggest_in_list3 L))
          (Smallest (smallest_in_list3 L)))
         (average Biggest Smallest))
 ;this is especially  useful if you are going to re-use the calculated intermediate value in multiple places. That way you don't have to re-calculate the value more than once.
+;If you aren't re-using any variables, then this technique is less efficient than normal lisp syntax, because the intermediate values are stored in variables instead of sitting on the stack ready to be consumed.
 
 (= 5 (pythonic_doit (@ List))) ;check that the average is 5
 
 
 and and and ;combine the 4 test results into a single true/false result.
+
+;0
